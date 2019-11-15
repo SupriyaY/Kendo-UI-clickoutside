125663a603aeb98e4998268b3c6b3223309539bb
Supriya Yerramilli <v-yerras@arconic.com>
2019-11-15 15:39:42 -0500

    clickout of dropdown function example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'my-app',
+template: `
+<div class="wrapper" style="height: 1000px">
+ <button class="show" #anchor>Show</button>
+ <kendo-popup *ngIf="show" [anchor]="anchor">
+     Popup content!
+ </kendo-popup>
+ </div>
+
+`
 })
 export class AppComponent {
-  title = 'kendoclickoutside';
+  public show: boolean = false;
+  @HostListener("click", ['$event']) onClick(e){
+    if(e.target.classList.contains('show') || e.target.classList.contains('k-popup')){
+      this.show = true;
+    }else{
+      this.show = false;
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
+import { NgModule }      from '@angular/core';
+import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PopupModule } from '@progress/kendo-angular-popup';
 
-import { AppComponent } from './app.component';
+
+
+import { AppComponent }   from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, BrowserAnimationsModule, PopupModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
+
